5309c0620773f71079a81919d7f3deaf15e8b975
Zoey <791275445@qq.com>
2024-04-17 09:02:06 +0800

    first commit

---
+++ src/main/java/com/zy/User.java
@@ -0,0 +1,11 @@
+package com.zy;
+
+/**
+ * @author zouying
+ * @Date 2024/4/17
+ **/
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hello git in IDEA");
+    }
+}
