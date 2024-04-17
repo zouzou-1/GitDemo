a343c09e7545ce5e3579d3537682b6596c46b91a
Zoey <791275445@qq.com>
2024-04-17 09:09:56 +0800

    third commit by hot-fix

--- src/main/java/com/zy/User.java
+++ src/main/java/com/zy/User.java
@@ -8,5 +8,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("hello git in IDEA");
         System.out.println("hello git in IDEA 2");
+        System.out.println("hello git in IDEA hot-fix");
     }
 }
